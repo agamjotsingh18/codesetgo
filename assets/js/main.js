6c8bae62d3b5419c6eadca6b46235bedce9f5118
arora-jasjot <jasjotsinghchugh@gmail.com>
2022-03-06 22:55:17 +0530

    Optimised Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-/**
- * Template Name: Reveal - v4.5.0
- * Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function() {
     "use strict";
 
-    /**
-     * Easy selector helper function
-     */
+    // Selector Function
+
     const select = (el, all = false) => {
         el = el.trim()
         if (all) {
@@ -20,9 +13,7 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
+    // Listener Function
     const on = (type, el, listener, all = false) => {
         let selectEl = select(el, all)
         if (selectEl) {
@@ -34,16 +25,12 @@
         }
     }
 
-    /**
-     * Easy on scroll event listener 
-     */
+    // Scroll Event Listener
     const onscroll = (el, listener) => {
         el.addEventListener('scroll', listener)
     }
 
-    /**
-     * Navbar links active state on scroll
-     */
+    // Navbar Links
     let navbarlinks = select('#navbar .scrollto', true)
     const navbarlinksActive = () => {
         let position = window.scrollY + 200
@@ -61,9 +48,7 @@
     window.addEventListener('load', navbarlinksActive)
     onscroll(document, navbarlinksActive)
 
-    /**
-     * Scrolls to an element with header offset
-     */
+    // Scroll To
     const scrollto = (el) => {
         let header = select('#header')
         let offset = header.offsetHeight
@@ -75,9 +60,7 @@
         })
     }
 
-    /**
-     * Header fixed top on scroll
-     */
+    // Header Fixed to top
     let selectHeader = select('#header')
     if (selectHeader) {
         let headerOffset = selectHeader.offsetTop
@@ -95,9 +78,7 @@
         onscroll(document, headerFixed)
     }
 
-    /**
-     * Back to top button
-     */
+    //  Back to Top
     let backtotop = select('.back-to-top')
     if (backtotop) {
         const toggleBacktotop = () => {
@@ -111,18 +92,13 @@
         onscroll(document, toggleBacktotop)
     }
 
-    /**
-     * Mobile nav toggle
-     */
+    // Mobile Nav
     on('click', '.mobile-nav-toggle', function(e) {
         select('#navbar').classList.toggle('navbar-mobile')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
     })
 
-    /**
-     * Mobile nav dropdowns activate
-     */
     on('click', '.navbar .dropdown > a', function(e) {
         if (select('#navbar').classList.contains('navbar-mobile')) {
             e.preventDefault()
@@ -130,9 +106,7 @@
         }
     }, true)
 
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
+    // Scroll with offset
     on('click', '.scrollto', function(e) {
         if (select(this.hash)) {
             e.preventDefault()
@@ -148,9 +122,6 @@
         }
     }, true)
 
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
     window.addEventListener('load', () => {
         if (window.location.hash) {
             if (select(window.location.hash)) {
@@ -159,9 +130,8 @@
         }
     });
 
-    /**
-     * Clients Slider
-     */
+    // Swiper
+
     new Swiper('.hero-slider', {
         speed: 1000,
         loop: true,
@@ -172,100 +142,6 @@
         }
     });
 
-    /**
-     * Clients Slider
-     */
-    new Swiper('.clients-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            480: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            640: {
-                slidesPerView: 4,
-                spaceBetween: 20
-            },
-            992: {
-                slidesPerView: 6,
-                spaceBetween: 20
-            }
-        }
-    });
-
-    /**
-     * Porfolio isotope and filter
-     */
-    window.addEventListener('load', () => {
-        let portfolioContainer = select('.portfolio-container');
-        if (portfolioContainer) {
-            let portfolioIsotope = new Isotope(portfolioContainer, {
-                itemSelector: '.portfolio-item',
-                layoutMode: 'fitRows'
-            });
-
-            let portfolioFilters = select('#portfolio-flters li', true);
-
-            on('click', '#portfolio-flters li', function(e) {
-                e.preventDefault();
-                portfolioFilters.forEach(function(el) {
-                    el.classList.remove('filter-active');
-                });
-                this.classList.add('filter-active');
-
-                portfolioIsotope.arrange({
-                    filter: this.getAttribute('data-filter')
-                });
-                portfolioIsotope.on('arrangeComplete', function() {
-                    AOS.refresh()
-                });
-            }, true);
-        }
-
-    });
-
-    /**
-     * Initiate portfolio lightbox 
-     */
-    const portfolioLightbox = GLightbox({
-        selector: '.portfolio-lightbox'
-    });
-
-    /**
-     * Portfolio details slider
-     */
-    new Swiper('.portfolio-details-slider', {
-        speed: 400,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        }
-    });
-
-    /**
-     * Testimonials slider
-     */
     new Swiper('.testimonials-slider', {
         speed: 600,
         loop: true,
@@ -292,9 +168,8 @@
         }
     });
 
-    /**
-     * Animation on scroll
-     */
+    // AOS
+
     window.addEventListener('load', () => {
         AOS.init({
             duration: 1000,
@@ -305,6 +180,8 @@
     });
 
 })()
+
+// Responsive Testimonial Height
 
 const setTestimonialSize = () => {
     var maxHeight = 0;
