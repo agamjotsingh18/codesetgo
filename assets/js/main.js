d102b9d01b1decf0b49090a52b51caac47639207
Darshan <darshankedare1815@gmail.com>
2023-06-12 13:28:21 +0530

    changed submit btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,6 +201,22 @@ window.addEventListener('resize', () => {
     setTestimonialSize()
 });
 
+let button = document.querySelector('.button');
+let buttonText = document.querySelector('.tick');
+
+const tickMark = "<svg width=\"58\" height=\"45\" viewBox=\"0 0 58 45\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#fff\" fill-rule=\"nonzero\" d=\"M19.11 44.64L.27 25.81l5.66-5.66 13.18 13.18L52.07.38l5.65 5.65\"/></svg>";
+
+buttonText.innerHTML = "Submit";
+
+button.addEventListener('click', function() {
+
+  if (buttonText.innerHTML !== "Submit") {
+    buttonText.innerHTML = "Submit";
+  } else if (buttonText.innerHTML === "Submit") {
+    buttonText.innerHTML = tickMark;
+  }
+  this.classList.toggle('button__circle');
+});
 //send the content of enquiry form to the email
 function sendMail() {
     var tempParams = {
@@ -219,6 +235,7 @@ function sendMail() {
         alert('Form Submission Faild! Try Again');
     })
 }
+
 
 // ONCLICK FLIP CARD FOR SERVICE SECTION
 const boxes = document.querySelectorAll('#services .col-lg-6 .box');
