69c20d530fa4a443a9ee6f06d84c12b4920b621a
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-06-22 12:33:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -325,3 +325,22 @@ const boxes = document.querySelectorAll('#services .col-lg-6 .box');
         box.classList.toggle('is-flipped');
     })
 })
+
+
+//Gallery
+
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
