788cbf4bf92792ce7ee1a3ff639ec60ab55bdbcf
khadar1020 <khadarvsk@gmail.com>
2023-06-29 16:14:51 +0530

    added-jimages

--- assets/js/cheatsheets.js
+++ assets/js/cheatsheets.js
@@ -1,35 +1,42 @@
 const repodata = [
     {
+      image: "assets/img/programming-language-images/c-logo.png",
       heading: "C",
       description: "C programming language cheatsheet",
       link: "/assets/cheatsheets/C.pdf",
     },
     {
+      image: "assets/img/programming-language-images/java-logo.png",
       heading: "Java",
       description: "Java programming language cheatsheet",
       link: "https://introcs.cs.princeton.edu/java/11cheatsheet/",
     },
     {
+        image: "assets/img/programming-language-images/html-logo.png",
         heading: "HTML",
         description: "HTML cheatsheet",
         link: "https://htmlcheatsheet.com/",
       },
       {
+        image: "assets/img/programming-language-images/css-logo.png",
         heading: "CSS",
         description: "CSS cheatsheet",
         link: "https://htmlcheatsheet.com/css/",
       },
       {
+        image: "assets/img/programming-language-images/javascript-logo.png",
         heading: "JavaScript",
         description: "JavaScript programming language cheatsheet",
         link: "https://htmlcheatsheet.com/js/",
       },
     {
+      image: "assets/img/programming-language-images/c++-logo.png",
       heading: "C++",
       description: "C++ programming language cheatsheet",
       link: "./assets/cheatsheets/CPP.pdf",
     },
     {
+      image: "assets/img/programming-language-images/python-logo.png",
       heading: "Python",
       description: "Python programming language cheatsheet",
       link: "./assets/cheatsheets/Python.pdf",
@@ -43,6 +50,8 @@ const repodata = [
   allOptions = repodata.map(
     (option) =>
       `<div class="option">
+       <img class="option-image" 
+       src="${option.image}">
         <h1 class="cheatsheet-heading">${option.heading}</h1>
         <p class="option-description">
           ${option.description}
